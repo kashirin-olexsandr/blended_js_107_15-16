936b6d5562005770b906f53b32647fc18c1e5bb9
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-05-26 10:29:08 +0200

    task-2

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -18,3 +18,34 @@ function getTime(min) {
 }
 
 console.log(getTime(20));
+
+// Task-2
+
+//~ Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+//~ значення '1', то у змінну result запишемо 'зима', якщо має значення
+//~ '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+
+const num = 5;
+
+function getSeason(num) {
+  let result = "";
+  switch (num) {
+    case 1:
+      result = "зима";
+      break;
+    case 2:
+      result = "весна";
+      break;
+    case 3:
+      result = "літо";
+      break;
+    case 4:
+      result = "осень";
+      break;
+    default:
+      result = "введене невірне значення";
+  }
+  return result;
+}
+
+console.log(getSeason(num));
