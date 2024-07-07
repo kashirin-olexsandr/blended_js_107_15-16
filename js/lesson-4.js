99bec4946b81ca03136eec8f14563dc96216be8c
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-07-07 12:15:17 +0200

    task-4

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -1,8 +1,6 @@
-/*
-Завдання 2
-Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
-Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
-*/
+// Завдання 1
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
 
 const swapButton = document.querySelector("#swapButton");
 const leftInput = document.querySelector("#leftSwapInput");
@@ -14,12 +12,10 @@ swapButton.addEventListener("click", (event) => {
   rightInput.value = temp;
 });
 
-/*
-Завдання 2
-Кнопка "Приховати" ховає текст і замінює назву кнопки на
-"Розкрити", при повторному натисканні текст знову стає доступним
-і кнопка набуває початкового вигляду.
-*/
+// Завдання 2
+// Кнопка "Приховати" ховає текст і замінює назву кнопки на
+// "Розкрити", при повторному натисканні текст знову стає доступним
+// і кнопка набуває початкового вигляду.
 
 const passwordButton = document.querySelector("#passwordButton");
 const passwordInput = document.querySelector("#passwordInput");
@@ -35,7 +31,6 @@ passwordButton.addEventListener("click", (event) => {
 
 // Завдання 3
 // Кнопка "Зменшити" робить квадрат менше на 10 пікселів, допопка "Збільшити" - більше на 10 пікселів.
-// */
 
 const box = document.querySelector("#box");
 const decrease = document.querySelector("#decrease");
@@ -66,3 +61,19 @@ decrease.addEventListener("click", () => {
   box.style.width = currentWidth + "px";
   box.style.height = currentHeight + "px";
 });
+
+// Завдання 6
+// Натиснувши кнопку "Подвоювати", збільшити значення
+// у кожному елементі списку у 2 рази
+
+const listItems = document.querySelectorAll(".listItem");
+console.log(listItems);
+const btn = document.querySelector("#double");
+function doubleCallClick() {
+  listItems.forEach((item) => {
+    console.log(item.textContent);
+    const currentValue = parseInt(item.textContent);
+    item.textContent = currentValue * 2;
+  });
+}
+btn.addEventListener("click", doubleCallClick);
